cd8565ee980a62021bf1d981fc562ed1a35df24e
Olivier Huin <olih@users.noreply.github.com>
2021-08-21 14:24:56 +0100

    Generate French web page

---
+++ generator/flarebyte/french-data.ts
@@ -0,0 +1,20 @@
+export const french = {
+  title: "Flarebyte.com Ltd | Applications web performantes en temps et en heure",
+  copyright: "Copyright (c) 2008-2021 Flarebyte.com Limited",
+  contentLanguage: "fr-fr",
+  lang: "fr",
+  keywords:
+    "programmation logicielle, Java, Python, London, Java Enterprise,Java EE, Django, mobile devices, scalable",
+  description:
+    "Flarebyte.com Ltd réalise des applications web basées sur Java Enterprise ou le framework python Django Python",
+  billboard: "Développement d'applications web performantes en temps et en heure",
+  about: "A propos de Flarebyte.com Ltd",
+  deck: "FFlarebyte.com Ltd est une start-up specialisée dans le développement d'applications web",
+  services: [
+    "Contenu interactif",
+    "Sites web pour téléphones portables",
+    "Développement logiciel selon un cahier des charges client",
+    "Conseil en architecture logicielle",
+    "Gestion de projet",
+  ]
+};
--- generator/flarebyte/generate.ts
+++ generator/flarebyte/generate.ts
@@ -1,9 +1,10 @@
 import { readFileSync, writeFileSync, PathLike } from "fs";
 import { english } from "./english-data";
 import { compile } from "handlebars";
-import {Converter } from 'showdown'
+import { Converter } from "showdown";
+import { french } from "./french-data";
 
-const mdPath = 'generator/flarebyte/md'
+const mdPath = "generator/flarebyte/md";
 
 const readFileSyncAsString = (path: PathLike) => readFileSync(path, "utf8");
 
@@ -11,21 +12,26 @@ const writeFileSyncAsString = (path: PathLike, content: string) =>
   writeFileSync(path, content, "utf8");
 
 const readMdFileAsHtml = (path: PathLike): string => {
-  const md = readFileSyncAsString(path)
-  const converter = new Converter()
-  return converter.makeHtml(md)
-}
-
-const generate = () => {
-  const template = readFileSyncAsString("generator/flarebyte/main-page-template.hbs");
-  const compiled = compile(template);
-  const md = {
-    servicesInfo: readMdFileAsHtml(`${mdPath}/services-info.md`),
-    contact: readMdFileAsHtml(`${mdPath}/contact.md`),
-  }
-  const englishAndMd = { ...english, ...md}
-  const html = compiled(englishAndMd);
-  writeFileSyncAsString('flarebyte_com/index.html', html)
+  const md = readFileSyncAsString(path);
+  const converter = new Converter();
+  return converter.makeHtml(md);
 };
 
-generate()
+const generate = (language: "english" | "french", metadata: object) => {
+  const template = readFileSyncAsString(
+    "generator/flarebyte/main-page-template.hbs"
+  );
+  const compiled = compile(template);
+  const md = {
+    servicesInfo: readMdFileAsHtml(`${mdPath}/${language}/services-info.md`),
+    contact: readMdFileAsHtml(`${mdPath}/${language}/contact.md`),
+  };
+  const metaAndMd = { ...metadata, ...md };
+  const html = compiled(metaAndMd);
+  const langFolder =
+    language === "english" ? "flarebyte_com" : "flarebyte_com/fr";
+  writeFileSyncAsString(`${langFolder}/index.html`, html);
+};
+
+generate("english", english);
+generate("french", french);
